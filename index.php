8d6bb9e76f47368f21bc0fb406bf377e4520f864
Achraf Amimi <amimi.achraf@hotmail.fr>
2017-03-07 11:57:24 +0100

    add CSS

--- index.php
+++ index.php
@@ -3,7 +3,21 @@
 <head>
 	<title>git</title>
 </head>
+	<style type="text/css">
+		h1{
+			position: absolute;
+			top: 40%;
+			width: 20%;
+			left: 40%;
+			border-bottom-left-radius: 10px;
+			border-top-right-radius: 10px;
+			text-align: center;
+			color: white;
+			font-family: Helvetica;
+			background-color: #446CB3;
+		}
+	</style>
 <body>
-<h1>hello git</h1>
+<h1>Hello Git</h1>
 </body>
 </html>
